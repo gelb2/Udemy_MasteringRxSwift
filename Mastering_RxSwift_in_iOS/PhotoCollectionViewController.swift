ca42a5a450ef4a9ef9f73fde77a2e14fe336a4fd
JunYoungJee <rainmaker8492@gmail.com>
2021-12-07 19:31:26 +0900

    22. fetching all photo assets

--- Mastering_RxSwift_in_iOS/PhotoCollectionViewController.swift
+++ Mastering_RxSwift_in_iOS/PhotoCollectionViewController.swift
@@ -10,6 +10,8 @@ import Photos
 
 class PhotoCollectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populatePhotos()
@@ -17,9 +19,18 @@ class PhotoCollectionViewController: UICollectionViewController {
     
     private func populatePhotos() {
         //request permission
-        PHPhotoLibrary.requestAuthorization { status in
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
             if status == .authorized {
                 //access photos from photo library
+                let asset = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                asset.enumerateObjects { asset, count, stop in
+                    self?.images.append(asset)
+                }
+                
+                //sort in reversed order
+                self?.images.reverse()
+//                self?.collectionView.reloadData()
             } else {
                 
             }
