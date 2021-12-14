3f9cc9fef631e75da28ddcc208a71cb2c4b70827
JunYoungJee <rainmaker8492@gmail.com>
2021-12-14 20:18:09 +0900

    25. Subscribing and Displaying the Selected Photo

--- Mastering_RxSwift_in_iOS/ViewController.swift
+++ Mastering_RxSwift_in_iOS/ViewController.swift
@@ -10,12 +10,28 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController
+                , let photoCollectionViewController = navC.viewControllers.first as? PhotoCollectionViewController else {
+                    return
+                }
+        photoCollectionViewController.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            self?.photoImageView.image = photo
+        })
+            .disposed(by: disposeBag)
     }
 
 
