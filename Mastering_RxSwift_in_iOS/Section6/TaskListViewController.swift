c1133238b816f8ba750512ae461423376bd41b94
JunYoungJee <rainmaker8492@gmail.com>
2021-12-16 10:14:57 +0900

    38. Designing the task list screen

--- Mastering_RxSwift_in_iOS/Section6/TaskListViewController.swift
+++ Mastering_RxSwift_in_iOS/Section6/TaskListViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class TaskListViewController: UIViewController {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,4 +34,24 @@ class TaskListViewController: UIViewController {
     }
     */
 
+}
+
+extension TaskListViewController: UITableViewDelegate {
+    
+}
+
+extension TaskListViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
 }
