1b7ba9fbc46c7c62f3913d8810e87d0b2e0aa0b1
JunYoungJee <rainmaker8492@gmail.com>
2021-12-22 18:32:09 +0900

    95. populating and displaying news in uitableview

--- Mastering_RxSwift_in_iOS/Section12/MVVMNewsTableViewController.swift
+++ Mastering_RxSwift_in_iOS/Section12/MVVMNewsTableViewController.swift
@@ -12,6 +12,7 @@ import RxCocoa
 class MVVMNewsTableViewController: UITableViewController {
     
     private let disposeBag = DisposeBag()
+    private var articleListVM: ArticleListViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +41,37 @@ class MVVMNewsTableViewController: UITableViewController {
         let resource = Resource<MVVMArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=cd1d5fbfb7be4a46bfc824991225619c")!)
         
         URLRequest.load(resource: resource)
-            .subscribe(onNext: { value in
-                print(value)
+            .subscribe(onNext: { [weak self] articleResponse in
+                let articles = articleResponse?.articles
+                self?.articleListVM = ArticleListViewModel(articles ?? [])
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
             })
             .disposed(by: disposeBag)
         
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM == nil ? 0: self.articleListVM.articlesVM.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MVVMArticleCell", for: indexPath) as? MVVMArticleCell else { fatalError() }
+        
+        let articleVM = self.articleListVM.articleAt(indexPath.row)
+        articleVM.title.asDriver(onErrorJustReturn: "")
+            .drive(cell.titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        articleVM.description.asDriver(onErrorJustReturn: "")
+            .drive(cell.descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        return cell
     }
 }
