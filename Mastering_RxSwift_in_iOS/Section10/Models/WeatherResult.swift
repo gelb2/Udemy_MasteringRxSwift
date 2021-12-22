a89b7e335650ef4b867b289263aace3230937f36
JunYoungJee <rainmaker8492@gmail.com>
2021-12-22 15:55:48 +0900

    74.displaying weather information

--- Mastering_RxSwift_in_iOS/Section10/Models/WeatherResult.swift
+++ Mastering_RxSwift_in_iOS/Section10/Models/WeatherResult.swift
@@ -11,6 +11,13 @@ struct WeatherResult: Decodable {
     let main: Weather
 }
 
+extension WeatherResult {
+    //EmptyWeather
+    static var empty: WeatherResult {
+        return WeatherResult(main: Weather(temp: 0.0, humidity: 0.0))
+    }
+}
+
 struct Weather: Decodable {
     let temp: Double
     let humidity: Double
--- Mastering_RxSwift_in_iOS/Section10/WeatherViewController.swift
+++ Mastering_RxSwift_in_iOS/Section10/WeatherViewController.swift
@@ -6,17 +6,21 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class WeatherViewController: UIViewController {
     
     @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
+    
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         drawViewController()
-        
+        bind()
         // Do any additional setup after loading the view.
     }
     
@@ -35,6 +39,45 @@ class WeatherViewController: UIViewController {
         
         humidityLabel.textColor = .lightGray
         humidityLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
+    }
+    
+    private func bind() {
+        self.cityNameTextField.rx.value
+            .subscribe(onNext: { city in
+                if let city = city {
+                    if city.isEmpty {
+                        self.displayWeather(nil)
+                    } else {
+                        self.fetchWeather(by: city)
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    private func displayWeather(_ weather: Weather?) {
+        if let weather = weather {
+            self.temperatureLabel.text = "\(weather.temp) "
+            self.humidityLabel.text = "\(weather.humidity) "
+        } else {
+            self.temperatureLabel.text = "No data"
+            self.humidityLabel.text = "NO Data"
+        }
+    }
+    
+    private func fetchWeather(by city: String) {
+        guard let cityEncoded = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+              let url = URL.urlForWeatherAPI(city: cityEncoded) else {
+                  return
+              }
+        let resource = Resource<WeatherResult>(url: url)
+        URLRequest.load(resource: resource)
+            .observeOn(MainScheduler.instance) //Making Sure event is observed on Main Thread, not background Thread
+            .catchErrorJustReturn(WeatherResult.empty)
+            .subscribe(onNext: { [weak self] value in
+                let weather = value?.main
+                self?.displayWeather(weather)
+            })
+            .disposed(by: disposeBag)
     }
     
 
